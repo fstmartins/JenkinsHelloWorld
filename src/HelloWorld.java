b311e2fd645e78fc37cfb44375b93eef9b69279e
Fábio Martins <30468937+fstmartins@users.noreply.github.com>
2025-03-27 12:08:09 +0000

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("-----***** Trying webhook 1*****-----");
+    System.out.println("-----***** Trying webhook 2*****-----");
   }
 }
